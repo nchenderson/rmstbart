b83ce570409537d2096bf1e6a607afab72f8a9de
Nicholas Henderson <nchender@umich.edu>
2025-03-03 20:11:34 -0500

    Deleted many unnecessary files and changed name of main function

--- R/RMST_BART.R
+++ R/RMST_BART.R
@@ -1,112 +1,122 @@
 
-RMST_BART <- function(
-    U, delta, x.train, Gweights, x.test=matrix(0.0,0,0),
-    tau=NULL, sgrid=NULL, sigma.mu=NULL,
+rmstbart <- function(
+    times, delta, x.train, Gweights, x.test=matrix(0.0,0,0),
+    tau=NULL, sgrid=NULL,
     transformation="identity",
     sparse=FALSE, theta=0, omega=1,
     a=0.5, b=1, augment=FALSE, rho=NULL,
     xinfo=matrix(0.0,0,0), usequants=TRUE,
     cont=FALSE, rm.const=TRUE,
-    k=2.0, power=2.0, base=.95, sigmaf=NA,
+    kappa=2.0, power=2.0, base=.95, sigmaf=NA,
     ntree=200L, numcut=100L,
     ndpost=1000L, nskip=100L, keepevery=1L,
     nkeeptrain=ndpost, nkeeptest=ndpost,
     nkeeptestmean=ndpost, nkeeptreedraws=ndpost,
     printevery=100L) {
-  ## xmat is the matrix that only contains the rows of
-  ## X where delta=1
-  nd0 <- sum(delta==0)
-  if(ncol(x.train) > 1) {
-    xmat <- x.train[delta==1,]
-    xmat_d0 <- x.train[delta==0,]
-  } else{
-    xmat <- matrix(x.train[delta==1,], nrow=sum(delta==1), ncol=1)
-    colnames(xmat) <- colnames(x.train)
 
-    xmat_d0 <- matrix(x.train[delta==0,], nrow=sum(delta==0), ncol=1)
-    colnames(xmat_d0) <- colnames(x.train)
-  }
-
-  kk <- k
-  temp = bartModelMatrix(xmat, numcut, usequants=usequants,
-                         cont=cont, xinfo=xinfo, rm.const=rm.const)
-  x.train = t(temp$X)
-  numcut = temp$numcut
-  xinfo = temp$xinfo
-  testset_used <- FALSE
-  if(length(x.test)>0 & nd0 > 0) {
-    x.tmp <- rbind(xmat_d0, x.test)
-    x.test <- bartModelMatrix(x.tmp)
-    x.test <- t(x.test[ , temp$rm.const])
-    testset_used <- TRUE
-  } else if(length(x.test)==0 & nd0 > 0) {
-    x.test <- bartModelMatrix(xmat_d0)
-    x.test <- t(x.test[ , temp$rm.const])
-  } else if(length(x.test)>0 & nd0 == 0) {
-    x.test <- bartModelMatrix(x.test)
-    x.test <- t(x.test[ , temp$rm.const])
-    testset_used <- TRUE
-  }
-  rm.const <- temp$rm.const
-  grp <- temp$grp
-  rm(temp)
-
-  n <- ncol(x.train)
-  p <- nrow(x.train)
-  np <- ncol(x.test)
-  if(length(rho)==0) rho=p
-  if(length(rm.const)==0) rm.const <- 1:p
-  if(length(grp)==0) grp <- 1:p
-
-  Gweights <- c(t(Gweights))
-  if(length(Gweights) != n*(ndpost*keepevery + nskip + 1)) {
-      stop("Gweights does not have the correct dimensions")
-  }
-  ## As a default, set tau to maximum of observed survival times
-  if(is.null(tau)) {
-    ## what to do if all delta==0?
-    tau <- max(U[delta==1])
-  }
-  U_tau <- pmin(U[delta==1], tau)
-
-  if(is.null(sgrid)) {
-    sgrid <- seq(0, tau, length.out=100)
-  }
-
-  ## Get KM estimate of censoring distribution and KM inverse censoring weights
-  KM_cens <- survfit(Surv(U, 1 - delta) ~ 1)
-  GKMfn <- stepfun(c(0, KM_cens$time), c(1, KM_cens$surv, min(KM_cens$surv)))
-  GKM_weights <- GKMfn(U_tau)
-  if(sum(GKM_weights == 0) > 0) {
-     GKM_weights[GKM_weights==0] <- min(KM_cens$surv[KM_cens$surv > 0])
-  }
-
-  ## Define centered version of U_tau
-  ## and set sigma.mu if its values was not input to the function
-  if(transformation=="identity") {
-    ## compute muhatb
-    muhatb <- mean(U_tau/GKM_weights)
-    Y_tau <- U_tau - muhatb
-    if(is.null(sigma.mu)) {
-      Ymin <- min(U_tau)
-      sigma.mu <- (tau - muhatb - Ymin)/(2*kk*sqrt(ntree))
+    ### First check if all observations are censored.
+    nd0 <- sum(delta==0)
+    if(nd==nrow(x.train)) {
+        stop("All observations are censored")
     }
-  } else if(transformation=="log") {
-    ## compute muhatb
-    muhatb <- mean(log(U_tau)/GKM_weights)
-    Y_tau <- log(U_tau) - muhatb
-    if(is.null(sigma.mu)) {
-      Ymin <- min(U_tau)
-      sigma.mu <- (log(tau) - muhatb - log(Ymin))/(2*kk*sqrt(ntree))
-    }
-  }
 
-  ptm <- proc.time()
-  #call
-  ## is a vector of weights here.
-  nu <- 3
-  lambda <- 1.0
-  sigest <- 1.0
+    ## Create matrix xmat. This will be the matrix that only
+    ## contains the rows of x.train where events occur (i.e. delta=1)
+    if(ncol(x.train) > 1) {
+        xmat <- x.train[delta==1,]
+        xmat_d0 <- x.train[delta==0,]
+    } else{
+        xmat <- matrix(x.train[delta==1,], nrow=sum(delta==1), ncol=1)
+        colnames(xmat) <- colnames(x.train)
+
+        xmat_d0 <- matrix(x.train[delta==0,], nrow=sum(delta==0), ncol=1)
+        colnames(xmat_d0) <- colnames(x.train)
+    }
+
+    ## Create model matrix tailored for use by BART:
+    temp = bartModelMatrix(xmat, numcut, usequants=usequants,
+                           cont=cont, xinfo=xinfo, rm.const=rm.const)
+
+    x.train = t(temp$X)
+    numcut = temp$numcut
+    xinfo = temp$xinfo
+    testset_used <- FALSE
+    if(length(x.test)>0 & nd0 > 0) {
+        ## If censoring is present, add censored obs to the input
+        ## test set
+        x.tmp <- rbind(xmat_d0, x.test)
+        x.test <- bartModelMatrix(x.tmp)
+        x.test <- t(x.test[ , temp$rm.const])
+        testset_used <- TRUE
+     } else if(length(x.test)==0 & nd0 > 0) {
+        ## If censoring is present and no test set is entered,
+        ## only treat censoring observations as test set
+        x.test <- bartModelMatrix(xmat_d0)
+        x.test <- t(x.test[ , temp$rm.const])
+     } else if(length(x.test)>0 & nd0 == 0) {
+        x.test <- bartModelMatrix(x.test)
+        x.test <- t(x.test[ , temp$rm.const])
+        testset_used <- TRUE
+     }
+     rm.const <- temp$rm.const
+     grp <- temp$grp
+     rm(temp)
+
+     n <- ncol(x.train)
+     p <- nrow(x.train)
+     np <- ncol(x.test)
+     if(length(rho)==0) rho=p
+     if(length(rm.const)==0) rm.const <- 1:p
+     if(length(grp)==0) grp <- 1:p
+
+     Gweights <- c(t(Gweights))
+     if(length(Gweights) != n*(ndpost*keepevery + nskip + 1)) {
+         stop("Gweights does not have the correct dimensions")
+     }
+     ## As a default, set tau to maximum of observed survival times
+     if(is.null(tau)) {
+         ## what to do if all delta==0?
+         tau <- max(U[delta==1])
+     }
+     U_tau <- pmin(U[delta==1], tau)
+
+     #if(is.null(sgrid)) {
+     #     sgrid <- seq(0, tau, length.out=100)
+     #}
+
+     ## Get KM estimate of censoring distribution and KM inverse censoring weights
+     KM_cens <- survfit(Surv(U, 1 - delta) ~ 1)
+     GKMfn <- stepfun(c(0, KM_cens$time), c(1, KM_cens$surv, min(KM_cens$surv)))
+     GKM_weights <- GKMfn(U_tau)
+     if(sum(GKM_weights == 0) > 0) {
+         GKM_weights[GKM_weights==0] <- min(KM_cens$surv[KM_cens$surv > 0])
+     }
+
+     ## Define centered version of U_tau
+     ## and set sigma.mu if its values was not input to the function
+     if(transformation=="identity") {
+        ## compute muhatb
+        muhatb <- mean(U_tau/GKM_weights)
+        Y_tau <- U_tau - muhatb
+
+        Ymin <- min(U_tau)
+        sigma.mu <- (tau - muhatb - Ymin)/(2*kappa*sqrt(ntree))
+
+     } else if(transformation=="log") {
+        ## compute muhatb
+        muhatb <- mean(log(U_tau)/GKM_weights)
+        Y_tau <- log(U_tau) - muhatb
+
+        Ymin <- min(U_tau)
+        sigma.mu <- (log(tau) - muhatb - log(Ymin))/(2*kappa*sqrt(ntree))
+     }
+
+     ptm <- proc.time()
+     #call
+     ## is a vector of weights here.
+     nu <- 3
+     lambda <- 1.0
+     sigest <- 1.0
 
   res = .Call("cwbart",
               n,  #number of observations in training data
